1fa909bdd7fda3d6b8a1f58c591b7c4dd73dd333
ateucher <andy.teucher@gmail.com>
2013-09-25 13:19:19 -0700

    Assignment1 after group chat

--- R/Assignment1.R
+++ R/Assignment1.R
@@ -1,42 +1,52 @@
 # Download and load the data
-temp <- tempfile()
+data_url <- "http://spark-public.s3.amazonaws.com/compdata/data/hw1_data.zip"
+zipfile <- "./data/hw1_data.zip"
 
-download.file("http://spark-public.s3.amazonaws.com/compdata/data/hw1_data.zip"
-              , destfile=temp)
+download.file(data_url, destfile=zipfile)
 
-data <- read.csv(unz(temp, filename="hw1_data.csv"), stringsAsFactors=FALSE)
-unlink(temp); rm(temp)
+data <- read.csv(unzip(zipfile, exdir="./data"), stringsAsFactors=FALSE)
 
-#Save the data
-write.csv(data, file="./data/hw1_data.csv", row.names=FALSE)
+# These data are from the included 'airquality' dataset:
+identical(data,airquality)
 
-# Question 1:
+# Question 1
+names(data)
 colnames(data)
 
-# Question 2:
-head(data,2)
+# Question 2
+head(data,n=2)
+data[1:2,]
 
-# Question 3:
+# Question 3
+str(data)
 nrow(data)
 
-# Question 4:
+# Question 4
 tail(data,2)
+data[(nrow(data)-1):nrow(data),]
+data[-(1:(nrow(data)-2)),]
 
-# Question 5:
-data[47,"Ozone"]
+# Question 5
+data[47,"Ozone"] # Index by column name
+data[47,1] # Index by column number
 
-# Question 6:
+# Question 6
 nrow(data[is.na(data$Ozone),])
 
-# Question 7:
-mean(data$Ozone,na.rm=TRUE)
+# Question 7
+mean(data$Ozone, na.rm=TRUE)
+mean(data$Ozone[!is.na(data$Ozone)])
 
-# Question 8:
-subset.q8 <- data[data$Ozone > 31 & data$Temp > 90,]
-mean(subset.q8$Solar.R, na.rm=TRUE)
+# Question 8
+subset <- data[data$Ozone > 31 & data$Temp > 90,]
+mean(subset$Solar.R, na.rm=TRUE)
 
-# Question 9:
-mean(data$Temp[data$Month==6], na.rm=TRUE)
+subset2 <- subset(data, Ozone > 31 & Temp > 90, drop=TRUE)
+mean(subset2$Solar.R)
 
-# Question 10:
+# Question 9
+mean(data[data$Month == 6,"Temp"], na.rm=TRUE)
+mean(data$Temp[data$Month == 6], na.rm=TRUE)
+
+# Question 10
 max(data$Ozone[data$Month == 5], na.rm=TRUE)
